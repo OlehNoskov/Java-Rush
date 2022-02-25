442f45d826f8f83b71b31d19f2e299390c1f0592
OlehNoskov <olega6000@gmail.com>
2022-02-25 11:13:57 +0200

    Completing tasks

--- javarush/src/main/java/ua/com/javarush/multithreading/inner_classes/mvc/model/ModelData.java
+++ javarush/src/main/java/ua/com/javarush/multithreading/inner_classes/mvc/model/ModelData.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class ModelData {
 
     private List<User> users;
+    private User activeUser;
 
     public List<User> getUsers() {
         return users;
@@ -14,5 +15,13 @@ public class ModelData {
 
     public void setUsers(List<User> users) {
         this.users = users;
+    }
+
+    public User getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        this.activeUser = activeUser;
     }
 }
---
+++ javarush/src/main/java/ua/com/javarush/multithreading/inner_classes/mvc/view/EditUserView.java
@@ -0,0 +1,28 @@
+package ua.com.javarush.multithreading.inner_classes.mvc.view;
+
+import ua.com.javarush.multithreading.inner_classes.mvc.bean.User;
+import ua.com.javarush.multithreading.inner_classes.mvc.controller.Controller;
+import ua.com.javarush.multithreading.inner_classes.mvc.model.ModelData;
+
+import java.util.List;
+
+public class EditUserView implements View{
+
+    private Controller controller;
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited!");
+        List<User> users = modelData.getUsers();
+
+        for (User user : users) {
+            System.out.println("\t" + user);
+        }
+        System.out.println("=========================");
+    }
+}
