7bc374934986b7b5f1f52f209b6759e348ba955c
OlehNoskov <olega6000@gmail.com>
2022-03-15 19:41:50 +0200

    Completing tasks

---
+++ javarush/src/main/java/ua/com/javarush/chat_project/client/ClientGuiController.java
@@ -0,0 +1,73 @@
+package ua.com.javarush.chat_project.client;
+
+public class ClientGuiController extends Client {
+
+    ClientGuiModel model = new ClientGuiModel();
+    //Передача обьекта ClientGuiController в конструктор ClientGuiView
+    ClientGuiView view = new ClientGuiView(this);
+
+    public static void main(String[] args) {
+        ClientGuiController controller = new ClientGuiController();
+        controller.run();
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new GuiSocketThread();
+    }
+
+    public ClientGuiModel getModel() {
+        return model;
+    }
+
+    @Override
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.run();
+    }
+
+    @Override
+    protected String getServerAddress() {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort() {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName() {
+        return view.getUserName();
+    }
+
+    @Override
+    protected void sendTextMessage(String message) {
+         super.sendTextMessage(message);
+    }
+
+    public class GuiSocketThread extends SocketThread {
+        @Override
+        protected void processIncomingMessage(String message) {
+            //Выводим текст сообщения
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        protected void informAboutAddingNewUser(String userName) {
+            //выводим информацию о добавлении нового юзера
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            //выводим информацию о удалении12 нового юзера
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
+}
