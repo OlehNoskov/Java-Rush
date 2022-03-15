fa62d5329c3b852e8c1853afed2ccb5aa243613c
OlehNoskov <olega6000@gmail.com>
2022-03-15 20:07:33 +0200

    Completing tasks

---
+++ javarush/src/main/java/ua/com/javarush/cycles/SecondDeadLock.java
@@ -0,0 +1,27 @@
+package ua.com.javarush.cycles;
+
+public class SecondDeadLock {
+
+    private final Object lock = new Object();
+
+    public synchronized void firstMethod() {
+        synchronized (lock) {
+            doSomething();
+        }
+    }
+
+    public void secondMethod() {
+        synchronized (lock) {
+            synchronized (this) {
+                doSomething();
+            }
+        }
+    }
+
+    private void doSomething() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ javarush/src/main/java/ua/com/javarush/cycles/ThirdDeadLock.java
@@ -0,0 +1,41 @@
+package ua.com.javarush.cycles;
+
+public class ThirdDeadLock {
+
+    private final String field;
+
+    public ThirdDeadLock(String field) {
+        this.field = field;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public synchronized void sout(ThirdDeadLock solution) {
+        System.out.format("111:  %s: %s %n", this.field, solution.getField());
+        solution.sout2(this);
+    }
+
+    public synchronized void sout2(ThirdDeadLock solution) {
+        System.out.format("222:  %s: %s %n", this.field, solution.getField());
+        solution.sout(this);
+    }
+
+    public static void main(String[] args) {
+        final ThirdDeadLock solution = new ThirdDeadLock("first");
+        final ThirdDeadLock solution2 = new ThirdDeadLock("second");
+        new Thread(new Runnable() {
+            public void run() {
+                solution.sout(solution2);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                solution2.sout(solution);
+            }
+        }).start();
+
+    }
+}
