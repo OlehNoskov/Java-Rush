a28aee03641f0668488120625f99b6ed31930b74
OlehNoskov <olega6000@gmail.com>
2022-03-18 21:09:52 +0200

    Completing tasks

---
+++ javarush/src/main/java/ua/com/javarush/archiver/ZipFileManager.java
@@ -0,0 +1,15 @@
+package ua.com.javarush.archiver;
+
+import java.nio.file.Path;
+
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip (Path source) throws Exception{
+
+    }
+}
