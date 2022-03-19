b0fd16270ff539a041e0d460274b853557b70531
OlehNoskov <olega6000@gmail.com>
2022-03-19 21:34:30 +0200

    Completing tasks

--- javarush/src/main/java/ua/com/javarush/archiver/Archiver.java
+++ javarush/src/main/java/ua/com/javarush/archiver/Archiver.java
@@ -1,22 +1,31 @@
 package ua.com.javarush.archiver;
 
-import ua.com.javarush.archiver.command.ExitCommand;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Archiver {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        System.out.println("Введите полный путь файла архива:");
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(bis.readLine()));
+        } while (operation != Operation.EXIT);
+    }
 
-        System.out.println("Введите полное имя файла для архивации:");
-        zipFileManager.createZip(Paths.get(bis.readLine()));
-
-        new ExitCommand().execute();
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы");
+        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать файл");
+        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
+        //Возврат Operation enum по введенному номеру!
+            return Operation.values()[ConsoleHelper.readInt()];
     }
 }
