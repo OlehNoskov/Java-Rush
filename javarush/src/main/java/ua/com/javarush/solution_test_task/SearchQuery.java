5115f8ab8976250b899e3913b819135197225c48
OlehNoskov <olega6000@gmail.com>
2022-05-18 18:07:48 +0300

    work with test task

--- javarush/src/main/java/ua/com/javarush/solution_test_task/SearchQuery.java
+++ javarush/src/main/java/ua/com/javarush/solution_test_task/SearchQuery.java
@@ -4,35 +4,40 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class SearchQuery {
     //    private static final String fileNameInputData = "input.txt";
     private static final String fileNameInputData = "C:\\Users\\Олег и Ирина\\IdeaProjects\\Java Rush\\javarush\\src\\main\\java\\ua\\com\\javarush\\solution_test_task\\input.txt";
-    private static final List<String> listValidRequest = new ArrayList<>();
-    private static final List<String> listValidWaitTimeline = new ArrayList<>();
+    private static final Map<String, Integer> listValidRequest = new LinkedHashMap();
+    private static final Map<String, Integer> listValidWaitTimeline = new LinkedHashMap<>();
 
     public void addValidRequestsAndWaitTimeline() {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileNameInputData))) {
             String line = reader.readLine();
+            int index = 1;
             while (line != null) {
                 if (ValidQuery.isQuery(line)) {
-                    listValidRequest.add(line);
+                    listValidRequest.put(line, index);
                 }
                 if (WaitTimelineValid.isValidWaitTimeline(line)) {
-                    listValidWaitTimeline.add(line);
+                    listValidWaitTimeline.put(line, index);
                 }
                 line = reader.readLine();
+                index++;
             }
-            for (String s : listValidRequest) {
-                System.out.println(s);
-            }
+            listValidRequest.forEach((key, value) -> {;
+                System.out.println(key + "=" + value + " ");
+            });
             System.out.println();
 
-            for(String s: listValidWaitTimeline){
-                System.out.println(s);
-            }
+            listValidWaitTimeline.forEach((key, value) -> {;
+                System.out.println(key + "=" + value + " ");
+            });
         } catch (FileNotFoundException e) {
             System.out.println("Sorry, file not found!");
         } catch (IOException e) {
--- javarush/src/main/java/ua/com/javarush/solution_test_task/Solution.java
+++ javarush/src/main/java/ua/com/javarush/solution_test_task/Solution.java
@@ -5,7 +5,5 @@ public class Solution {
     public static void main(String[] args) {
         SearchQuery searchQuery = new SearchQuery();
         searchQuery.addValidRequestsAndWaitTimeline();
-//        String array = "D 1.1 8.15.1 P 15.10.2012 83 ";
-        String array = "D 1.1 10.1.2 P 15.10.2012 83";
     }
 }
