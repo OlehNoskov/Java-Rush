52727835cff682920ec4b51dd1510a73b8043220
OlehNoskov <olega6000@gmail.com>
2022-06-04 14:55:25 +0300

    work with custom tree

---
+++ javarush/src/main/java/ua/com/javarush/my_tree/CustomTree.java
@@ -0,0 +1,66 @@
+package ua.com.javarush.my_tree;
+
+import java.io.Serializable;
+
+import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+
+    @Override
+    public String get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    static class Entry<T> implements Serializable {
+        String elementName;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        public boolean isAvailableToAddChildren() {
+            return this.availableToAddLeftChildren || this.availableToAddRightChildren;
+        }
+    }
+}
---
+++ javarush/src/main/java/ua/com/javarush/my_tree/Solution.java
@@ -0,0 +1,9 @@
+package ua.com.javarush.my_tree;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+    }
+}
