7843a8e737b0629853045f7e2f0c8eb53ab36a1f
OlehNoskov <olega6000@gmail.com>
2022-07-01 19:08:33 +0300

    Java Rush custom HashSet

--- javarush/src/main/java/ua/com/javarush/amigo_set/AmigoSet.java
+++ javarush/src/main/java/ua/com/javarush/amigo_set/AmigoSet.java
@@ -1,5 +1,7 @@
 package ua.com.javarush.amigo_set;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import java.util.*;
@@ -13,23 +15,89 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Serializable,
     }
 
     public AmigoSet(Collection<? extends E> collection) {
-        this.map = new HashMap<>();
+        this.map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
         map.putAll((Map<? extends E, ?>) collection);
     }
 
-    public boolean add() {
-        boolean result;
-
-        return false;
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    //Поверхностное клоирование
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        try {
+            AmigoSet<E> newSet = (AmigoSet<E>) super.clone();
+            newSet.map = (HashMap<E, Object>) map.clone();
+            return newSet;
+        } catch (Exception e) {
+            throw new InternalError();
+        }
+    }
+
+    private void writeObject(ObjectOutputStream s) throws java.io.IOException {
+        // Write out any hidden serialization magic
+        s.defaultWriteObject();
+
+        // Write out HashMap capacity and load factor
+        s.writeInt(HashMapReflectionHelper.<Integer>callHiddenMethod(map, "capacity"));
+        s.writeFloat(HashMapReflectionHelper.<Float>callHiddenMethod(map, "loadFactor"));
+
+        // Write out size
+        s.writeInt(map.size());
+
+        // Write out all elements in the proper order.
+        for (E e : map.keySet())
+            s.writeObject(e);
+    }
+
+
+    private void readObject(ObjectInputStream s) throws java.io.IOException, ClassNotFoundException {
+        // Read in any hidden serialization magic
+        s.defaultReadObject();
+
+        // Read in HashMap capacity and load factor and create backing HashMap
+        int capacity = s.readInt();
+        float loadFactor = s.readFloat();
+        map = new HashMap<>(capacity, loadFactor);
+
+        // Read in size
+        int size = s.readInt();
+
+        // Read in all elements in the proper order.
+        for (int i = 0; i < size; i++) {
+            E e = (E) s.readObject();
+            map.put(e, PRESENT);
+        }
     }
 }
---
+++ javarush/src/main/java/ua/com/javarush/amigo_set/HashMapReflectionHelper.java
@@ -0,0 +1,19 @@
+package ua.com.javarush.amigo_set;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class HashMapReflectionHelper {
+    public static <T> T callHiddenMethod(HashMap map, String methodName) {
+        try {
+            Method method = map.getClass().getDeclaredMethod(methodName);
+            method.setAccessible(true);
+            return (T) method.invoke(map);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException ignored) {
+        }
+        return null;
+    }
+}
