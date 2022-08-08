d3d5cd9184ec0b9460aeb2b12b75e461515211ca
OlehNoskov <olega6000@gmail.com>
2022-08-08 15:17:58 +0300

    Java Rush Topic: Date

---
+++ javarush/src/main/java/ua/com/javarush/other/TestMap.java
@@ -0,0 +1,31 @@
+package ua.com.javarush.other;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMap{
+    private static Map<User, String> users = new HashMap<>();
+
+    public static void main(String[] args) {
+        User user1 = new User(1, "Oleg", "Noskov");
+        User user2 = new User(1, "Oleg", "Noskov");
+        User user3 = new User(1, "Oleg", "Noskov");
+        users.put(user1, "Trainee");
+        users.put(user2, "Junior");
+        users.put(user3, "Middle");
+
+        System.out.println(users.get(user2));
+    }
+
+    private static class User {
+        int index;
+        private String name;
+        private String surname;
+
+        public User(int index, String name, String surname) {
+            this.index = index;
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+}
