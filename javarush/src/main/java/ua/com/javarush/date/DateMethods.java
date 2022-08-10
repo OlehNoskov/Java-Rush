ae051013fa070069cd6da2ca42cc938571b17bf1
OlehNoskov <olega6000@gmail.com>
2022-08-10 17:42:11 +0300

    Java Rush Topic: Date

---
+++ javarush/src/main/java/ua/com/javarush/date/DateMethods.java
@@ -0,0 +1,45 @@
+package ua.com.javarush.date;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+
+public class DateMethods {
+    public static void main(String[] args) {
+        System.out.println(isLeap(LocalDate.of(2000, 10, 10)));
+        System.out.println("=====================================");
+
+        System.out.println(isBefore(LocalDateTime.of(2030, 10, 20, 10, 10, 10)));
+        System.out.println(isBefore(LocalDateTime.of(2000, 10, 20, 10, 10, 10)));
+        System.out.println("=====================================");
+
+        System.out.println(addTime(LocalTime.now(), 10, ChronoUnit.HOURS));
+        System.out.println("=====================================");
+
+        System.out.println(getPeriodBetween(LocalDate.of(2000, 10, 20)
+                , LocalDate.of(2010, 12, 22)));
+    }
+
+    // Год высокосный?
+    public static boolean isLeap(LocalDate date) {
+        return date.isLeapYear();
+    }
+
+    // Дата предшевствует текущей дате?
+    public static boolean isBefore(LocalDateTime dateTime) {
+        return dateTime.isBefore(LocalDateTime.now());
+    }
+
+    // Возвращает время увеличенное на n chronoUnites
+    public static LocalTime addTime(LocalTime time, int n, ChronoUnit chronoUnit) {
+        return time.plus(n, chronoUnit);
+    }
+
+    // Возвращает временной промежутоке между датами
+    public static Period getPeriodBetween(LocalDate firstDate, LocalDate secondDate) {
+        return Period.between(firstDate, secondDate);
+    }
+}
--- javarush/src/main/java/ua/com/javarush/other/TestMap.java
+++ javarush/src/main/java/ua/com/javarush/other/TestMap.java
@@ -3,18 +3,19 @@ package ua.com.javarush.other;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TestMap{
+ class TestMap {
     private static Map<User, String> users = new HashMap<>();
 
     public static void main(String[] args) {
-        User user1 = new User(1, "Oleg", "Noskov");
-        User user2 = new User(1, "Oleg", "Noskov");
-        User user3 = new User(1, "Oleg", "Noskov");
-        users.put(user1, "Trainee");
-        users.put(user2, "Junior");
-        users.put(user3, "Middle");
+            User user1 = new User(1, "Oleg", "Noskov");
+            User user2 = new User(1, "Oleg", "Noskov");
+            User user3 = new User(1, "Oleg", "Noskov");
+            users.put(user1, "Trainee");
+            users.put(user2, "Junior");
+            users.put(user3, "Middle");
 
         System.out.println(users.get(user2));
+        System.out.println(testFinally());
     }
 
     private static class User {
@@ -26,6 +27,16 @@ public class TestMap{
             this.index = index;
             this.name = name;
             this.surname = surname;
+        }
+    }
+
+    private static int testFinally() {
+        try {
+            return 1;
+        } catch (Exception e) {
+            return 2;
+        } finally {
+            return 3;
         }
     }
 }
