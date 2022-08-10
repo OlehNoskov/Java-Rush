be9716d9f343dd82f4a4b0abf7c4026d350e8608
OlehNoskov <olega6000@gmail.com>
2022-08-10 17:50:58 +0300

    Java Rush Topic: Date

---
+++ javarush/src/main/java/ua/com/javarush/date/DateTimeAPI.java
@@ -0,0 +1,15 @@
+package ua.com.javarush.date;
+
+public class DateTimeAPI {
+    public static void main(String[] args) {
+        printDate("21.4.2014 15:56:45");
+        System.out.println();
+        printDate("21.4.2014");
+        System.out.println();
+        printDate("17:33:40");
+    }
+
+    public static void printDate(String date) {
+        //напишите тут ваш код
+    }
+}
